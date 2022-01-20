57af4f6679a2a2e763ce1892562d1425bce8ad12
Atlas205 <78223903+Atlas205@users.noreply.github.com>
2022-01-19 23:24:57 -0600

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,40 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    var x = document.getElementById("button");
+    if (x.innerHTML === "dark mode") {
+      x.innerHTML = "light mode";
+    } else {
+      x.innerHTML = "dark mode";
+    }
+ }
